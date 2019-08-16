8a36c5980e24edaf915d0afad1611c2ebbe9213c
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2019-08-16 08:40:19 -0400

    Countdown Component - Preparations

--- client/components/countdown.js
+++ client/components/countdown.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
+import {Button, ButtonToolbar} from 'react-bootstrap'
 
 export default class Countdown extends Component {
     constructor(props) {
         super(props)
     
         this.state = {
-            message: 'You\'re almost there, Jan. Hold on baby ko...'
+            message: 'You\'re almost there, Jan. Hold on baby ko...',
+            targetTitle: 'Freedom from NYCRN',
+            targetDate: '10-17-2019',
+            'sub-goal': [
+                {
+                    message: 'You\'re about to hang in your resignation yay!',
+                    targetTitle: 'Road to 4 weeks',
+                    targetDate: '09-10-2019'
+                }
+            ]
         }
     }
     
@@ -16,6 +26,9 @@ export default class Countdown extends Component {
                     <div className="intro col-md-12">
                         <p>You're almost there, Jan. Hold on baby ko...</p>
                     </div>
+                </div>
+                <div className="row">
+                    {/* Place Countdown elements here */}
                 </div>
             </div>
         )
