33b2b1a98ffe974a048b05e9c0dd7b47253fa885
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2019-08-26 12:16:22 -0400

    Completed Goal Backend

--- server/api/goal.js
+++ server/api/goal.js
@@ -1,13 +1,12 @@
 const router = require('express').Router()
 const {Goal} = require('../db/models')
-const db = require('../db')
-const Sequelize = require('sequelize')
+module.exports = router
+// const db = require('../db')
+// const Sequelize = require('sequelize')
 
 router.get('/', async (req, res, next) => {
     try {
-        const goals = await Goal.findAll({
-            id: 1
-        })
+        let goals = await Goal.findAll()
         console.log(goals)
         res.json(goals)
     } catch (error) {
