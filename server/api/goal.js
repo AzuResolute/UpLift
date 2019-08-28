2d2d6fd1434d5637c816caf0c151e4681315dcd8
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2019-08-27 22:11:49 -0400

    Expanding Express route for Goals

--- server/api/goal.js
+++ server/api/goal.js
@@ -4,11 +4,37 @@ module.exports = router
 // const db = require('../db')
 // const Sequelize = require('sequelize')
 
+// get all
 router.get('/', async (req, res, next) => {
     try {
         let goals = await Goal.findAll()
         res.json(goals)
     } catch (error) {
         next(error)
+    }
+})
+
+router.get('/id/:goalId', async (req, res, next) => {
+    try {
+        let goal = await Goal.findByPk(req.params.goalId)
+        res.json(goal)
+    } catch (error) {
+        next (error)
+    }
+})
+
+router.get('/id/:parentGoalId/expanded', async (req, res, next) => {
+    try {
+        let goal = {}
+        let {parentGoalId} = req.params
+        goal.parent = await Goal.findByPk(req.params.goalId)
+        goal.children = await Goal.findAll({
+            where: {
+                parentGoalId
+            }
+        })
+        res.json(goal)
+    } catch (error) {
+        next (error)
     }
 })
